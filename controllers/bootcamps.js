d5a06737644c6163c6e8b568b9cd639acb05cece
salahmander <salahabdo@hotmail.co.uk>
2021-01-20 23:58:26 +0000

    Update GET bootcamp route to fetch bootcamp by ID from database.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -5,9 +5,9 @@ const Bootcamp = require("../models/Bootcamps");
 // @access Public
 exports.getBootcamps = async (req, res, next) => {
   try {
-    const bootcamp = await Bootcamp.find();
+    const bootcamps = await Bootcamp.find();
 
-    res.status(200).json({ success: true, data: bootcamp });
+    res.status(200).json({ success: true, data: bootcamps });
   } catch (err) {
     res.status(400).json({ success: false });
   }
@@ -16,10 +16,19 @@ exports.getBootcamps = async (req, res, next) => {
 // @desc  Get all bootcamp
 // @route GET /api/v1/bootcamps/:id
 // @access Public
-exports.getBootcamp = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    // if id is correctly formatted but doesnt exist return 400
+    if (!bootcamp) {
+      return res.status(400).json({ sucess: false });
+    }
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc  Create new bootcamp
