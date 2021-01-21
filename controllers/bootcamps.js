1144f99c1c418041329a672d113a499d8fac676b
salahmander <salahabdo@hotmail.co.uk>
2021-01-21 21:34:27 +0000

    Update getBootcamps controller to include data filtering by selecting fields and sorting by fields.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -12,6 +12,13 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
   // Copy req.query
   const reqQuery = { ...req.query };
 
+  // Fields to exclude
+  const removeFields = ["select", "sort"];
+
+  // Loop over removeFields and delete them from reqQuery
+  removeFields.forEach((param) => delete reqQuery[param]);
+
+  // Create quary string
   let queryStr = JSON.stringify(reqQuery);
 
   // Create operators ($gt, $gte, etc)
@@ -22,6 +29,20 @@ exports.getBootcamps = asyncHandler(async (req, res, next) => {
 
   // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select fields
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+
+  // Sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort("-createdAt");
+  }
 
   // Executing query
   const bootcamps = await query;
