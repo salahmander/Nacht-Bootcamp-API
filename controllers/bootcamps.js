d28d3ee50cfbb65b75065b584affb4ef345e76d3
salahmander <salahabdo@hotmail.co.uk>
2021-01-21 21:00:37 +0000

    Update getBootcamps controller to include data filtering.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,7 +7,19 @@ const Bootcamp = require("../models/Bootcamps");
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+
+  let queryStr = JSON.stringify(req.query);
+
+  queryStr = queryStr.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+
+  query = Bootcamp.find(JSON.parse(queryStr));
+
+  const bootcamps = await query;
+  
   res.status(200).json({ success: true, data: bootcamps });
 });
 
@@ -90,12 +102,14 @@ exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
   const radius = distance / 3963;
 
   const bootcamps = await Bootcamp.find({
-    location: { $geoWithin: { $centerSphere: [[longitude, latitude], radius] } }
+    location: {
+      $geoWithin: { $centerSphere: [[longitude, latitude], radius] },
+    },
   });
 
   res.status(200).json({
     success: true,
     count: bootcamps.length,
-    data: bootcamps
+    data: bootcamps,
   });
 });
