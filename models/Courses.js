058fc50cfe28b79b22735cd8cc7047416e9d1a50
salahmander <salahabdo@hotmail.co.uk>
2021-01-23 15:20:07 +0000

    Create aggregation pipeline to calculate average tuition cost for courses.

--- models/Courses.js
+++ models/Courses.js
@@ -38,4 +38,34 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log("Calulating avg cost...".blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: {
+        bootcamp: bootcampId,
+      },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        avergaeCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+
+  console.log(obj);
+};
+
+// Call getAverageCost after save
+CourseSchema.post("save", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Cal getAVerageCost before remove
+CourseSchema.pre("remove", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
