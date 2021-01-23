052a6828c4bea59e8149ed2cdbfc78d95a95bb80
salahmander <salahabdo@hotmail.co.uk>
2021-01-23 10:59:33 +0000

    Create new controller for courses.

---
+++ controllers/courses.js
@@ -0,0 +1,25 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
+const Courses = require("../models/Courses");
+
+// @desc get courses
+// @route GET /api/v1/courses
+// @route GET /api/v1/bootcamps/:bootcampId/courses
+// @access Public
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  let query;
+
+  if (req.params.bootcampId) {
+    query = Courses.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = Courses.find();
+  }
+
+  const courses = await query;
+
+  res.status(200).json({
+    success: true,
+    count: courses.length,
+    data: courses,
+  });
+});
