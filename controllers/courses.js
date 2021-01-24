4f9205eb7bc8a6c1dfa1ff7cda4b52086ae09541
salahmander <salahabdo@hotmail.co.uk>
2021-01-24 14:22:38 +0000

    Add new advanced results middleware to getCourses controller.

--- controllers/courses.js
+++ controllers/courses.js
@@ -8,24 +8,17 @@ const Bootcamp = require("../models/Bootcamps");
 // @route GET /api/v1/bootcamps/:bootcampId/courses
 // @access Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-  let query;
-
   if (req.params.bootcampId) {
-    query = Courses.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Courses.find().populate({
-      path: "bootcamp",
-      select: "name description website",
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+
+    return res.status(200).json({
+      success: true,
+      count: courses.length,
+      data: courses,
     });
+  } else {
+    res.status(200).json(res.advancedResults);
   }
-
-  const courses = await query;
-
-  res.status(200).json({
-    success: true,
-    count: courses.length,
-    data: courses,
-  });
 });
 
 // @desc get course
