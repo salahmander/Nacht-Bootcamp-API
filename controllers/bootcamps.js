806b5ee011085c8fd9418ef8343feec4b2392854
salahmander <salahabdo@hotmail.co.uk>
2021-01-24 14:09:43 +0000

    Make getBootcamps controller use new middleware.

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,48 +8,7 @@ const Bootcamp = require("../models/Bootcamps");
 // @route GET /api/v1/bootcamps
 // @access Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-
-  // Copy req.query
-  const reqQuery = { ...req.query };
-
-  // Fields to exclude
-  const removeFields = ["select", "sort"];
-
-  // Loop over removeFields and delete them from reqQuery
-  removeFields.forEach((param) => delete reqQuery[param]);
-
-  // Create quary string
-  let queryStr = JSON.stringify(reqQuery);
-
-  // Create operators ($gt, $gte, etc)
-  queryStr = queryStr.replace(
-    /\b(gt|gte|lt|lte|in)\b/g,
-    (match) => `$${match}`
-  );
-
-  // Finding resource
-  // To limit the courses field pass in object to .populate with the fields you want
-  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
-
-  // Select fields
-  if (req.query.select) {
-    const fields = req.query.select.split(",").join(" ");
-    query = query.select(fields);
-  }
-
-  // Sort
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-
-  // Executing query
-  const bootcamps = await query;
-
-  res.status(200).json({ success: true, data: bootcamps });
+  res.status(200).json(res.advancedResults);
 });
 
 // @desc  Get all bootcamp
