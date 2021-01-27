98bdd4bf229b3457ab830d28e8eb3a9d71e7e1ff
salahmander <salahabdo@hotmail.co.uk>
2021-01-27 23:55:27 +0000

    Only allow Publisher and Admin to create, update and delete courses.

--- routes/courses.js
+++ routes/courses.js
@@ -13,7 +13,7 @@ const Course = require("../models/Courses");
 
 const advancedResults = require("../middleware/advancedResults");
 
-const { protect } = require("../middleware/auth");
+const { protect, authorize } = require("../middleware/auth");
 
 router
   .route("/")
@@ -24,8 +24,12 @@ router
     }),
     getCourses
   )
-  .post(protect, addCourse);
+  .post(protect, authorize("publisher", "admin"), addCourse);
 
-router.route("/:id").get(getCourse).put(protect, updateCourse).delete(protect, deleteCourse);
+router
+  .route("/:id")
+  .get(getCourse)
+  .put(protect, authorize("publisher", "admin"), updateCourse)
+  .delete(protect, authorize("publisher", "admin"), deleteCourse);
 
 module.exports = router;
