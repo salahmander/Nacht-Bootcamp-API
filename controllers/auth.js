6be5a2801b803fb5904e2854d9ea5f0bd3449441
salahmander <salahabdo@hotmail.co.uk>
2021-01-27 22:33:48 +0000

    Create new controller to retreive users data.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,6 @@
 const ErrorResponse = require("../utils/errorResponse");
 const asyncHandler = require("../middleware/async");
 const User = require("../models/User");
-const { options } = require("../routes/auth");
 
 // @desc  Get Register user
 // @route POST /api/auth/register
@@ -48,6 +47,18 @@ exports.login = asyncHandler(async (req, res, next) => {
   }
 
   sendTokenResponse(user, 200, res);
+});
+
+// @desc      Get current logged in user
+// @route     POST /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 });
 
 // Get token from model, create cookie and send response
