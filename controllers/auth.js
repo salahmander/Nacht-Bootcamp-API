c4a786c2d63fe4308b7d3307b82113f6479bc1d1
salahmander <salahabdo@hotmail.co.uk>
2021-01-27 22:16:29 +0000

    Small fix.

--- controllers/auth.js
+++ controllers/auth.js
@@ -53,7 +53,7 @@ exports.login = asyncHandler(async (req, res, next) => {
 // Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
-  const token = user.getSignedJwtToken;
+  const token = user.getSignedJwtToken();
 
   const options = {
     expires: new Date(
