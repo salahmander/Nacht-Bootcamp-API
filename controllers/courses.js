4151bee8abd3076ba60b697360b26bd04f3795fa
salahmander <salahabdo@hotmail.co.uk>
2021-01-28 17:16:08 +0000

    Check to see if user is the course owner before updating the course.

--- controllers/courses.js
+++ controllers/courses.js
@@ -84,7 +84,18 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 
   if (!course) {
     return next(
-      new ErrorResponse(`No course with the id ${req.params.id} exists`, 404)
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user.id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update course ${course._id}`,
+        401
+      )
     );
   }
 
