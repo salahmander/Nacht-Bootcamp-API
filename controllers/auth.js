5c255b2a1049769a69b977ef1a2656a39071420f
salahmander <salahabdo@hotmail.co.uk>
2021-01-29 13:46:28 +0000

    Create new controller to handle updating user details.

--- controllers/auth.js
+++ controllers/auth.js
@@ -128,6 +128,26 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc      Update user details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 // @desc      Reset password
 // @route     PUT /api/v1/auth/resetpassword/:resettoken
 // @access    Public
