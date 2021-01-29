bcf547846024fe73fcd02bf14ee94ada51739e74
salahmander <salahabdo@hotmail.co.uk>
2021-01-29 20:47:13 +0000

    Create new controller to handle creating a new user as admin.

--- controllers/users.js
+++ controllers/users.js
@@ -21,6 +21,18 @@ exports.getUser = asyncHandler(async (req, res, next) => {
   });
 });
 
+// @desc      Create user
+// @route     POST /api/v1/auth/users
+// @access    Private/Admin
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    data: user,
+  });
+});
+
 // @desc      Update user
 // @route     PUT /api/v1/auth/users/:id
 // @access    Private/Admin
