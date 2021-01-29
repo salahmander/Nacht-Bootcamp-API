67950fbb60eb19a0f005cfc644546d3574fa196f
salahmander <salahabdo@hotmail.co.uk>
2021-01-29 20:49:56 +0000

    Create new route to delete user as admin.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { getUsers, getUser } = require("../controllers/users");
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users");
 
 const User = require("../models/User");
 
@@ -13,6 +19,6 @@ router.use(authorize("admin"));
 
 router.route("/").get(advancedResults(User), getUsers).post(createUser);
 
-router.route("/:id").get(getUser).put(updateUser);
+router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
