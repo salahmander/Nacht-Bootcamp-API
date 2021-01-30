51a9d3b77dabbd6bd7a4cfe5faee69d7e9bf85f1
salahmander <salahabdo@hotmail.co.uk>
2021-01-30 13:05:09 +0000

    Create new route to fetch single review by id.

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getReviews } = require("../controllers/reviews");
+const { getReviews, getReview } = require("../controllers/reviews");
 
 const Review = require("../models/Reviews");
 
@@ -14,5 +14,7 @@ router.route("/").get(
   }),
   getReviews
 );
+
+router.route("/:id").get(getReview);
 
 module.exports = router;
